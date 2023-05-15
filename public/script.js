0f403bcf83e30d29839977b5870c81519bf33a7b
zenitba <112856019+zenitba@users.noreply.github.com>
2023-05-15 22:51:25 +0200

    Versie 2.0

--- public/script.js
+++ public/script.js
@@ -1,30 +1,40 @@
-let socket = io()
+/*JS berichten versturen*/
+let socket = ioServer()
 let messages = document.querySelector('section ul')
-let input = document.querySelector('input')
+let input = document.querySelector('#message')
+let handle = document.querySelector('#handle')
+let feedback =  document.querySelector('#feedback');
+let count =  document.querySelector('#count');
 
-document.querySelector('form').addEventListener('submit', (event) => {
+document.querySelector('form').addEventListener('submit', event => {
   event.preventDefault()
-  if (input.value) {
-    socket.emit('message', input.value)
-    input.value = ''
-  }
-})
-
-socket.on('message', (message) => {
-  addMessage(message)
-})
-
-socket.on('whatever', (message) => {
-  addMessage(message)
-})
-
-socket.on('history', (history) => {
-  history.forEach((message) => {
-    addMessage(message)
+    socket.emit('chat', {
+      input: input.value,
+      handle: handle.value,
+      time: new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) /*tijd bij de bericht*/
   })
+  input.value = ''
 })
-
-function addMessage(message) {
-  messages.appendChild(Object.assign(document.createElement('li'), { textContent: message }))
+/*tbericht verstuurd met gebruiksnaam en tijd*/
+socket.on('chat', data => {
+  feedback.innerHTML = ''
+  messages.innerHTML += `
+  <div class="message-background">
+    <p class="time">${data.time}</p>
+    <p>${data.input}</p>
+  </div>
+  <h3 class="message-handle">${data.handle}</h3>
+  `
   messages.scrollTop = messages.scrollHeight
-}
+})
+/*waneer gebruiker typt*/
+input.addEventListener('keypress', function(){
+  socket.emit('typing', handle.value)
+})
+/*melding waneer andere gebruiker typt*/
+socket.on('typing', data => {
+  feedback.innerHTML = `<p><em> ${data} typ een bericht...</em></p>`
+})
+socket.on('usercount', data => {
+  count.innerHTML = data;
+})
