f33ef81ecf21b933ad3c1b1217c122045e220113
zenitba <112856019+zenitba@users.noreply.github.com>
2023-05-15 12:36:58 +0200

    test

--- server.js
+++ server.js
@@ -31,7 +31,9 @@ let htmlMemberList = null
 
 // Serveer client-side bestanden
 app.use(express.static(path.resolve('public')))
-
+app.get('/', function (req, res) {
+    res.render('index', data)
+  })
 io.on('connection', (socket) => {
   // Log de connectie naar console
   console.log('a user connected')
